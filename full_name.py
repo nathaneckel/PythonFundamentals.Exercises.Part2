f6c454c11d565af76ebdd66b73afccfba91772b0
nathaneckel <nathan@patientpaperwork.com>
2024-03-13 14:44:39 -0400

    improvements - it will print now

--- full_name.py
+++ full_name.py
@@ -1,3 +1,5 @@
+#from Part 1
+
 first_name = "Nathan"
 last_name = "Eckel"
 full_name = (first_name + " " + last_name)
--- greet.py
+++ greet.py
@@ -1,12 +1,19 @@
-#1. Why no printout?
+#EXERCISE 2
+#defining greet function
+#printing custom greeting with name_input variable
+def greet(name_input):
+    print("Hello " + name_input + "!")
+name_input = input("Please enter your name. ")
 
-def greet():
-    myname = "Nathan Eckel"
-    name = "Darth Vader"
-    name2 = "Guido van Rossum"
-    print(myname, ",", name, ",", name2)
+#I still don't understand why this is necessary (it IS necessary, the logic isn't intuitive yet)
+greet("Nathan Eckel")
+greet("Darth Vader")
+greet("Guido van Rossum")
 
+#list of name2 name3 myname
+myname = "Nathan Eckel"
+name3 = "Darth Vader"
+name2 = "Guido van Rossum"
 
-    print ("Hello" + greet(myname) +"")
-
-#name input
+for i in range(501):
+    greet(name_input + " " + myname + " " + name2 + " " + name3 + " " + str(i))
