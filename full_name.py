235903e7f39f022e4c3436b45bf0c93bb33e976c
nathaneckel <nathan@patientpaperwork.com>
2024-03-13 13:51:41 -0400

    improvements

---
+++ full_name.py
@@ -0,0 +1,4 @@
+first_name = "Nathan"
+last_name = "Eckel"
+full_name = (first_name + " " + last_name)
+print(full_name)
---
+++ greet.py
@@ -0,0 +1,12 @@
+#1. Why no printout?
+
+def greet():
+    myname = "Nathan Eckel"
+    name = "Darth Vader"
+    name2 = "Guido van Rossum"
+    print(myname, ",", name, ",", name2)
+
+
+    print ("Hello" + greet(myname) +"")
+
+#name input
